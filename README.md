56b337d151aab73555b9d69b594ff6670ee02e0c
Elton FAN <chuangen@live.com>
2018-05-10 23:55:23 +0800

    Add license file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Aqara
+# Aqara DotNet SDK
+
+.NET Standard, 实现绿米网关局域网通信协议。https://opencloud.aqara.cn
+
+后续将安排时间更新使用说明。
--- src/Aqara/AssemblyVersion.cs
+++ src/Aqara/AssemblyVersion.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
@@ -11,7 +11,7 @@
 [assembly: AssemblyCopyright("\u00A9 2017 Elton FAN (eltonfan@live.cn, http://elton.io)")]
 [assembly: AssemblyTrademark("ELTON")]
 
-[assembly: AssemblyVersion("$VERSION$.0")]
-[assembly: AssemblyFileVersion("$VERSION$.$REV$")]
-[assembly: AssemblyInformationalVersion("v$VERSION$ Beta")]
-[assembly: AssemblyDefaultAlias("$HASH$ $TIMESTAMP$")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.14")]
+[assembly: AssemblyInformationalVersion("v1.0.0 Beta")]
+[assembly: AssemblyDefaultAlias("8d1589f0834aeba2d1f96a0af79a6e35ed39c5cc 2018-04-30T11:02:23+08:00")]
